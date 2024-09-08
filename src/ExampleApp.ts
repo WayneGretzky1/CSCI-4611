a68a1402573ba80d545f53e05de696bc2a298328
WayneGretzky1 <brockbye@outlook.com>
2024-09-08 15:43:21 -0500

    "Lec 1 CSCI 4611"

--- src/ExampleApp.ts
+++ src/ExampleApp.ts
@@ -9,6 +9,7 @@ import * as gfx from 'gophergfx'
 export class ExampleApp extends gfx.GfxApp
 {   
  
+    bgColor : gfx.Color = new gfx.Color(0.0,0.0,0.0);
     // --- Create the ExampleApp class ---
     constructor()
     {
@@ -21,12 +22,33 @@ export class ExampleApp extends gfx.GfxApp
     // --- Initialize the graphics scene ---
     createScene(): void 
     {
+        this.renderer.background = this.bgColor;
+        const myRect = gfx.Geometry2Factory.createRect(2.5, 1.5);
 
+
+        const myEyeL = gfx.Geometry2Factory.createRect(.5, 0.5);
+        myEyeL.position = new gfx.Vector2(-2.5, 2.5);
+        this.scene.add(myEyeL);
+
+
+        const myEyeR = gfx.Geometry2Factory.createRect(1.5, 0.5);
+        const myNose = gfx.Geometry2Factory.createRect(0.5, 0.5);
+        const myMouth = gfx.Geometry2Factory.createRect(1.5, 0.25);
+
+  
+        myEyeR.position = new gfx.Vector2(2.5, 2.5);
+
+        // this.scene.add(myRect);
+
+        this.scene.add(myEyeR);
+        this.scene.add(myNose);
+        this.scene.add(myMouth);
     }
 
     // --- Update is called once each frame by the main graphics loop ---
     update(deltaTime: number): void 
     {
-
+        this.bgColor.g += 0.01;
+        this.renderer.background = this.bgColor;
     }
 }
